13a232984364bafc792461c5b94e58680a46029a
Poornesh Ganji <poornesh666666@gmail.com>
2026-01-08 14:51:31 +0530

    Added changes in atm file

--- jan08/atm.py
+++ jan08/atm.py
@@ -3,7 +3,8 @@ withdraw = int(input("Enter amount to withdraw: "))
 if withdraw > balance:
     print("Insufficient funds")
 else:
-    if withdraw % 100 != 0:
-        print("Not a multiple of 100")
-    else:
+    if withdraw % 100 == 0:
         print(f"Remaining balance: {balance-withdraw}")
+    else:
+        print("Not a multiple of 100")
+        
